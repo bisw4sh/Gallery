3c567ec54ea71495eb7b3682edba53821fddcb35
Biswash Dhungana <spikeystona@gmail.com>
2024-12-07 22:53:04 +0545

    configs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "biswashdhungana.com.np",
+      },
+      {
+        protocol: "https",
+        hostname: "wrrtcadwmufkgehggjmf.supabase.co",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
