f6ac49728ec332e9ea21297e12910ecc8b2375e0
Biswash Dhungana <spikeystona@gmail.com>
2025-01-04 19:46:47 +0545

    components for auth sections

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,16 @@ import "./globals.css";
 import { IoIosAddCircle } from "react-icons/io";
 import { FaUserCircle } from "react-icons/fa";
 import Link from "next/link";
+import { createClient } from "@/utils/supabase/server";
+import { LogOut } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -21,11 +31,16 @@ export const metadata: Metadata = {
   description: "HomePage",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
   return (
     <html lang="en">
       <body
@@ -37,12 +52,36 @@ export default function RootLayout({
               Gallery
             </Link>
             <div className="flex justify-center items-center gap-1">
-              <Link href="/upload">
-                <IoIosAddCircle className="text-[40px] hover:fill-slate-500" />
-              </Link>
-              <Link href="/signin">
-                <FaUserCircle className="text-[36px] hover:fill-slate-500" />
-              </Link>
+              {user ? (
+                <Link href="/upload">
+                  <IoIosAddCircle className="text-[40px] hover:fill-slate-500" />
+                </Link>
+              ) : null}
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <Link href="/signin">
+                    <FaUserCircle className="text-[36px] hover:fill-slate-500" />
+                  </Link>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuLabel>
+                    {user?.email?.toString()}
+                  </DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>Profile</DropdownMenuItem>
+                  <DropdownMenuItem>Dashboard</DropdownMenuItem>
+                  <DropdownMenuItem className="bg-rose-700 cursor-pointer">
+                    {user ? (
+                      <Link
+                        className="flex items-center justify-between w-full"
+                        href="signout"
+                      >
+                        Sign Out <LogOut className="h-[1rem]" />
+                      </Link>
+                    ) : null}
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
             </div>
           </nav>
           {children}
