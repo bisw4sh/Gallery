599a8bf88794abc0e4c6c63b14dd3c210bead292
Biswash Dhungana <spikeystona@gmail.com>
2025-01-17 18:31:02 +0545

    feat: error page

--- components/ErrorAlert.tsx
+++ components/ErrorAlert.tsx
@@ -6,12 +6,16 @@ import { useSearchParams } from "next/navigation";
 export default function ErrorAlert() {
   const searchParams = useSearchParams();
   const error = searchParams.get("error");
+  const success = searchParams.get("success");
 
   useEffect(() => {
     if (error) {
       console.log("Error captured:", error);
       toast(error.toString());
+    } else if (success) {
+      console.log("Error captured:", success);
+      toast(success.toString());
     }
-  }, [error]);
+  }, [error, success]);
   return null;
 }
