6a954da1ef3aae6311611f07af3e9411f045c971
Biswash Dhungana <spikeystona@gmail.com>
2025-01-20 23:51:49 +0545

    feat: loader on the root of /panel

--- app/(admin)/panel/page.tsx
+++ app/(admin)/panel/page.tsx
@@ -1,7 +1,23 @@
-// import { createClient } from "@/utils/supabase/server";
+"use client";
+import { trefoil } from "ldrs";
 
-export default async function ApprovalDashboard() {
-  //   const supabase = await createClient();
-
-  return <main>Page</main>;
+export default function ApprovalDashboard() {
+  trefoil.register();
+  return (
+    <main className="w-full flex justify-center items-center h-[40rem] relative">
+      <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 mt-4 text-2xl font-bold">
+        Admin Panel
+      </h1>
+      <div className="flex justify-center items-center h-full">
+        <l-trefoil
+          size="400"
+          stroke="4"
+          stroke-length="0.15"
+          bg-opacity="0.1"
+          speed="1.4"
+          color="black"
+        ></l-trefoil>
+      </div>
+    </main>
+  );
 }
